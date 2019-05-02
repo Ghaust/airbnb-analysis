fcc9448cbde0ce005d5404fceffbf82fadd6c9da
Joseph-Emmanuel <josephE.banzio@gmail.com>
2019-05-02 23:20:04 +0200

    neighbourhood analysis done

--- neighbourhood_analysis.R
+++ neighbourhood_analysis.R
@@ -5,7 +5,15 @@ library(questionr) #we load the needed extensions
 paris.airbnb.listings <- read.csv("~/Desktop/Computer Science/Engineering School/2018-2019/S2/AirBnB_Analysis/paris-airbnb-listings.csv", header=TRUE, sep=";")
 View(paris.airbnb.listings) #visualization of the array
 
-cities <- table(paris.airbnb.listings$Neighbourhood)
-sort(cities) #trie selon la valeur de l'effectif
+neigh <- table(paris.airbnb.listings$Neighbourhood)
+sort(neigh) #trie selon la valeur de l'effectif
 
 freq(paris.airbnb.listings$Neighbourhood)
+
+barplot(sort(neigh), col = "gray",
+        main = "Taux de location des Airbnb en fonction du quartier",
+        xlab = "Ville",
+        ylab = "Effectif") #graphique en barres
+
+
+dotchart(sort(neigh)) #diagramme de Cleveland
